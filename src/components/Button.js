66ab1cbb9ee59069a962ddccb26b1b3367120a8d
ishpiamazing <ishpiamazing@gmail.com>
2020-09-28 18:43:16 -0400

    button component built in storybook env

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,32 @@ import React from "react";
 
 import "components/Button.scss";
 
+const classNames = require('classnames')
+
+// export default function Button(props) {
+
+//    let buttonClass = classNames({
+//       button: true,
+//       'button--confirm' : props.confirm,
+//       'button--danger': props.danger
+//    });
+
+//    return <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>;  
+// }
+
 export default function Button(props) {
-   return <></>;
-}
+   const buttonClass = classNames("button", {
+     "button--confirm": props.confirm,
+     "button--danger": props.danger
+   });
+ 
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
