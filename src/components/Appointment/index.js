4eb5c0307b01b6edd6c9834d2a57768c49375bd1
ishpiamazing <ishpiamazing@gmail.com>
2020-09-29 15:51:14 -0400

    files added

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "components/Appointment/styles.scss"
+
+const classNames = require('classnames')
+
+export default function Appointment (props) {
+  return (
+  <article className="appointment">
+  </article>
+  )
+}
