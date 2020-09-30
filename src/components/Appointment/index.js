e310d9bf75bfccca62615dc0a5bd2d66e3472f68
ishpiamazing <ishpiamazing@gmail.com>
2020-09-30 09:05:37 -0400

    Appointment components added

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
+
 import "components/Appointment/styles.scss"
 
 const classNames = require('classnames')
@@ -7,6 +11,13 @@ const classNames = require('classnames')
 export default function Appointment (props) {
   return (
   <article className="appointment">
+    <Header time={props.time}/>
+    {props.interview ? 
+    (<Show
+    student = {props.interview.student}
+    interviewer = {props.interview.interviewer}
+  />) :
+  (<Empty />)}
   </article>
   )
 }
