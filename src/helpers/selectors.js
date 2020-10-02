e34b9dc62e8f88126b41760198ac6936021ebdb9
ishpiamazing <ishpiamazing@gmail.com>
2020-10-02 10:17:53 -0400

    getInterviewersForDay added with test cases

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -4,12 +4,25 @@ const dayFound = state.days.find(stateday => day === stateday.name);
 if(!dayFound){
   return [];
 }
+
 const appointmentsFound= dayFound.appointments.map(appointment => {
   return state.appointments[appointment];
 });
 return appointmentsFound;
 }
 
+
+
+export function getInterviewersForDay(state, day) {
+  const dayFound = state.days.find(stateday => day === stateday.name);
+  if(!dayFound || !dayFound.interviewers || dayFound.interviewers.length === 0){
+    return [];
+  }
+  const interviewersFound = dayFound.interviewers.map(interviewer => {
+    return state.interviewers[interviewer];
+  });
+  return interviewersFound;
+}
 
 export function getInterview(state, interview) {
   if(!interview) {
