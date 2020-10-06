0af420f04a1023287ffb539d0f35051bf2c9fb59
ishpiamazing <ishpiamazing@gmail.com>
2020-10-06 17:50:04 -0400

    integration testing

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -54,7 +54,7 @@ function cancel() {
 
 
   return (
-  <article className="appointment">
+  <article className="appointment" data-testid="appointment">
     <Header time={props.time}/>
     {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
     {mode === SAVING && (<Status message="Saving..."></Status>)}
